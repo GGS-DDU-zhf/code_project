17513b5816f39114e8f1f5833f11832abf8d0a50
DESKTOP-R495L20\Administrator <1240293935@qq.com>
2024-04-11 17:14:03 +0800

    添加视频地址

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,3 +1,5 @@
+// 学习视频地址 https://www.bilibili.com/video/BV1Nw411x77m/?p=3&spm_id_from=pageDriver&vd_source=0cf98087bbb988fb3bb958be12a290fe
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
